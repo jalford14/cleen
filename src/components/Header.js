7ae393ca00735e1410c2bc62b63ecc2094dd00d5
Jimmy Alford <jimmy.alford@salesloft.com>
2020-05-22 11:32:11 -0400

    add skeleton for price points

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ class Header extends React.Component {
   render() {
     return (
       <div class="navbar">
-        <a class="home" href="#">Cleen</a>
+        <a class="home" href="localhost:3000">Cleen</a>
 
-        <a class="nav push" href="#">About</a>
-        <a class="nav">Get Started!</a>
+        <a class="nav push" href="localhost:3000">About</a>
+        <a class="nav" href="localhost:3000">Get Started!</a>
       </div>
     );
   }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import logo from '../logo.svg';
+import WashOption from '../components/WashOption.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WashOption
+          price="$10"
+          title="Basic Wash"
+          description="Come to our home! We'll wash the exterior of your car for only $10."
+        />
+        <WashOption
+          price="$15"
+          title="On-Demand Wash"
+          description="Let us come to you! The only thing we ask is that you have a hose we can use."
+        />
+        <WashOption
+          price="$15"
+          title="Interior Detailing"
+          description="Add this to any wash and we'll "
+        />
+        <WashOption
+          price="$20"
+          title="The Full Works"
+          description="The best value in PA! Get your exterior and interior cleaned for the price of a large pizza."
+        />
       </header>
     </div>
   );
