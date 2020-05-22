8fa6f4779f6f7499f8654ebf0e0b77818a597f4f
Jimmy Alford <jimmy.alford@salesloft.com>
2020-05-22 15:50:42 -0400

    add some basic routing

--- src/components/WashOption.js
+++ src/components/WashOption.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './css/WashOption.css'
+import WashForm from './WashForm'
+import { Link, Switch, Route } from "react-router-dom";
 
 class WashOption extends React.Component {
   render() {
     return (
       <div class="content-box" style={{background: this.props.color}}>
-        <a href="localhost:3000">
+        <Link to="/basic">
           <p class="price">{this.props.price}</p>
           <p class="title">{this.props.title}</p>
           <p class="description">{this.props.description}</p>
-        </a>
+        </Link>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 import App from './containers/App';
+import WashForm from './components/WashForm';
 import Header from './components/Header';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/basic" component={WashForm} />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
