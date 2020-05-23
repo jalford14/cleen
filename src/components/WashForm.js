bdaf4700ad7ac9a4b07f029f82947e30aa7a7fd5
Jimmy Alford <jimmy.alford@salesloft.com>
2020-05-22 23:09:48 -0400

    create method that handles form upon submission

--- src/components/WashForm.js
+++ src/components/WashForm.js
@@ -1,32 +1,57 @@
-import React from 'react';
-import './css/WashForm.css'
+import React from "react";
+import "./css/WashForm.css"
+import axios from "axios";
 import { Link, Switch, Route } from "react-router-dom";
 
 class WashForm extends React.Component {
+  handleSubmit(e){
+        e.preventDefault();
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const phone = document.getElementById("phone").value;
+        const notes = document.getElementById("notes").value;
+        axios({
+            method: "POST",
+            url:"http://localhost:5000/send",
+            data: {
+                name: name,
+                email: email,
+                phone: phone,
+                notes: notes
+            }
+        }).then((response)=>{
+            if (response.data.msg === "success"){
+                alert("Message Sent.");
+                this.resetForm()
+            } else if(response.data.msg === "fail"){
+                alert("Message failed to send.")
+            }
+        })
+    }
+
   render() {
     return (
       <div class="form-container">
         <p class="form-header">Contact Us</p>
 
-        <form class="wash-form">
+        <form class="wash-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
           <label class="form-control" />
             <span class="form-label">Name *</span>
-              <input class="form-input" type="text" />
+              <input class="form-input" id="name" type="text" />
       
           <label class="form-control" />
             <span class="form-label">Email *</span>
-              <input class="form-input" type="text" />
+              <input class="form-input" id="email" type="text" />
 
           <label class="form-control" />
             <span class="form-label">Phone</span>
-              <input class="form-input" type="text" />
+              <input class="form-input" id="phone" type="text" />
 
           <label class="form-control" />
             <span class="form-label">Additional Notes</span>
-              <textarea class="form-textarea" />
+              <textarea class="form-textarea" id="notes" />
 
           <input class="form-submit" type="submit" value="Submit" />
-
         </form>
       </div>
     );
