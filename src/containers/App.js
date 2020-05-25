9492efdd28a91592648cc6068e0145a612138bb5
Jimmy Alford <jimmy.alford@salesloft.com>
2020-05-25 10:36:34 -0400

    change single to double quotes

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import WashOption from '../components/WashOption.js'
-import './App.css';
+import React from "react";
+import WashOption from "../components/WashOption.js"
+import "./App.css";
 
 function App() {
   return (
