20a5f5424c329bc505b7d6980521b3365dccee3e
Jimmy Alford <jimmy.alford@salesloft.com>
2020-06-06 09:37:14 -0400

    add some color to the boxes

--- src/containers/About.js
+++ src/containers/About.js
@@ -9,7 +9,37 @@ class About extends React.Component {
           About Us
         </div>
         
-        <div class="profile-box">
+        <div class="profile-box" style={{background: "#75C2E6"}}>
+          <div class="img-container">
+            <img src={require("../assets/img/rachel.jpeg")} />
+          </div>
+          <div class="person-container">
+            <div class="person-name">
+              Rachel Alford
+            </div>
+            <div class="person-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+              do eiusmod tempor incididunt ut labore et dolore magna aliqua. i
+              Ut pharetra sit amet aliquam id diam maecenas ultricies.
+            </div>
+          </div>
+        </div>
+        <div class="profile-box" style={{background: "#6AE6C9"}}>
+          <div class="img-container">
+            <img src={require("../assets/img/rachel.jpeg")} />
+          </div>
+          <div class="person-container">
+            <div class="person-name">
+              Lauren Hunter
+            </div>
+            <div class="person-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+              do eiusmod tempor incididunt ut labore et dolore magna aliqua. i
+              Ut pharetra sit amet aliquam id diam maecenas ultricies.
+            </div>
+          </div>
+        </div>
+        <div class="profile-box" style={{background: "#FA8C9C"}}>
           <div class="img-container">
             <img src={require("../assets/img/jimmy.jpeg")} />
           </div>
@@ -18,7 +48,9 @@ class About extends React.Component {
               Jimmy Alford
             </div>
             <div class="person-description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut pharetra sit amet aliquam id diam maecenas ultricies.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+              do eiusmod tempor incididunt ut labore et dolore magna aliqua. i
+              Ut pharetra sit amet aliquam id diam maecenas ultricies.
             </div>
           </div>
         </div>
