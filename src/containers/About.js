2a2dc59ec3eb13819b94c50d00ad2c404d3a9193
Jimmy Alford <jimmy.alford@salesloft.com>
2020-06-05 23:20:04 -0400

    make some css changes to the about page

--- src/containers/About.js
+++ src/containers/About.js
@@ -4,14 +4,22 @@ import "../components/css/About.css"
 class About extends React.Component {
   render() {
     return(
-      <div>
-        <div class="form-header">
+      <div class="about-container">
+        <div class="about-header">
           About Us
         </div>
         
         <div class="profile-box">
           <div class="img-container">
             <img src={require("../assets/img/jimmy.jpeg")} />
+          </div>
+          <div class="person-container">
+            <div class="person-name">
+              Jimmy Alford
+            </div>
+            <div class="person-description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut pharetra sit amet aliquam id diam maecenas ultricies.
+            </div>
           </div>
         </div>
       </div>
