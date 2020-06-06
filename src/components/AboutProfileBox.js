be8859e9ea6b54fa8466fb825239372122321e1c
Jimmy Alford <jimmy.alford@salesloft.com>
2020-06-06 10:20:48 -0400

    break out profile boxes into components

---
+++ src/components/AboutProfileBox.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../components/css/AboutProfileBox.css"
+
+class AboutProfileBox extends React.Component {
+  render() {
+    return(
+      <div class="profile-box" style={{background: this.props.color}}>
+        <div class="img-container">
+          <img src={this.props.picture} />
+        </div>
+        <div class="person-container">
+          <div class="person-name">
+            {this.props.name}
+          </div>
+          <div class="person-description">
+            {this.props.description}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AboutProfileBox;
--- src/containers/About.js
+++ src/containers/About.js
@@ -1,5 +1,9 @@
 import React from "react";
+import AboutProfileBox from "../components/AboutProfileBox.js"
 import "../components/css/About.css"
+
+import rachel from "../assets/img/rachel.jpeg"
+import jimmy from "../assets/img/jimmy.jpeg"
 
 class About extends React.Component {
   render() {
@@ -8,52 +12,24 @@ class About extends React.Component {
         <div class="about-header">
           About Us
         </div>
-        
-        <div class="profile-box" style={{background: "#75C2E6"}}>
-          <div class="img-container">
-            <img src={require("../assets/img/rachel.jpeg")} />
-          </div>
-          <div class="person-container">
-            <div class="person-name">
-              Rachel Alford
-            </div>
-            <div class="person-description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-              do eiusmod tempor incididunt ut labore et dolore magna aliqua. i
-              Ut pharetra sit amet aliquam id diam maecenas ultricies.
-            </div>
-          </div>
-        </div>
-        <div class="profile-box" style={{background: "#6AE6C9"}}>
-          <div class="img-container">
-            <img src={require("../assets/img/rachel.jpeg")} />
-          </div>
-          <div class="person-container">
-            <div class="person-name">
-              Lauren Hunter
-            </div>
-            <div class="person-description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-              do eiusmod tempor incididunt ut labore et dolore magna aliqua. i
-              Ut pharetra sit amet aliquam id diam maecenas ultricies.
-            </div>
-          </div>
-        </div>
-        <div class="profile-box" style={{background: "#FA8C9C"}}>
-          <div class="img-container">
-            <img src={require("../assets/img/jimmy.jpeg")} />
-          </div>
-          <div class="person-container">
-            <div class="person-name">
-              Jimmy Alford
-            </div>
-            <div class="person-description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-              do eiusmod tempor incididunt ut labore et dolore magna aliqua. i
-              Ut pharetra sit amet aliquam id diam maecenas ultricies.
-            </div>
-          </div>
-        </div>
+        <AboutProfileBox
+          color="#FA8C9C"
+          picture={rachel}
+          name="Rachel Alford"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut pharetra sit amet aliquam id diam maecenas ultricies. Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Est sit amet facilisis magna etiam tempor orci. Gravida cum sociis natoque penatibus et magnis dis parturient montes. Sit amet mattis vulputate enim nulla. Porttitor leo a diam sollicitudin tempor. A iaculis at erat pellentesque. Morbi tristique senectus et netus et malesuada fames ac turpis. At urna condimentum mattis pell"
+        />
+        <AboutProfileBox
+          color="#FA8C9C"
+          picture={rachel}
+          name="Lauren Hunter"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut pharetra sit amet aliquam id diam maecenas ultricies. Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Est sit amet facilisis magna etiam tempor orci. Gravida cum sociis natoque penatibus et magnis dis parturient montes. Sit amet mattis vulputate enim nulla. Porttitor leo a diam sollicitudin tempor. A iaculis at erat pellentesque. Morbi tristique senectus et netus et malesuada fames ac turpis. At urna condimentum mattis pell"
+        />
+        <AboutProfileBox
+          color="#FA8C9C"
+          picture={jimmy}
+          name="Jimmy Alford"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut pharetra sit amet aliquam id diam maecenas ultricies. Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Est sit amet facilisis magna etiam tempor orci. Gravida cum sociis natoque penatibus et magnis dis parturient montes. Sit amet mattis vulputate enim nulla. Porttitor leo a diam sollicitudin tempor. A iaculis at erat pellentesque. Morbi tristique senectus et netus et malesuada fames ac turpis. At urna condimentum mattis pell"
+        />
       </div>
     );
   }
