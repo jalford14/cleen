e91a0b1c423d0d67a871a855caacca18219367fb
Jimmy Alford <jimmy.alford@salesloft.com>
2020-06-07 14:02:41 -0400

    simple changes to the about page

--- src/containers/About.js
+++ src/containers/About.js
@@ -13,13 +13,13 @@ class About extends React.Component {
           About Us
         </div>
         <AboutProfileBox
-          color="#FA8C9C"
+          color="#6AE6C9"
           picture={rachel}
           name="Rachel Alford"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut pharetra sit amet aliquam id diam maecenas ultricies. Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Est sit amet facilisis magna etiam tempor orci. Gravida cum sociis natoque penatibus et magnis dis parturient montes. Sit amet mattis vulputate enim nulla. Porttitor leo a diam sollicitudin tempor. A iaculis at erat pellentesque. Morbi tristique senectus et netus et malesuada fames ac turpis. At urna condimentum mattis pell"
         />
         <AboutProfileBox
-          color="#FA8C9C"
+          color="#75C2E6"
           picture={rachel}
           name="Lauren Hunter"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut pharetra sit amet aliquam id diam maecenas ultricies. Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Est sit amet facilisis magna etiam tempor orci. Gravida cum sociis natoque penatibus et magnis dis parturient montes. Sit amet mattis vulputate enim nulla. Porttitor leo a diam sollicitudin tempor. A iaculis at erat pellentesque. Morbi tristique senectus et netus et malesuada fames ac turpis. At urna condimentum mattis pell"
